b0df96f9b0849356098f2bd71da29adc5315d9e5
Ben <benhunsicker1@gmail.com>
2023-04-14 14:14:19 -0400

    Music puzzle triggers ariadne

--- Assets/Input/Puzzles/PianoPuzzle.cs
+++ Assets/Input/Puzzles/PianoPuzzle.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Yarn.Unity;
 
 public class PianoPuzzle : MonoBehaviour
 {
     public string curSong;
     public string desiredSong;
+    public Transform ariadnePos;
+    private bool done = false;
 
 
     // Start is called before the first frame update
@@ -28,6 +32,18 @@ public class PianoPuzzle : MonoBehaviour
         if (curSong == desiredSong)
         {
             GetComponent<AudioSource>().Play();
+            StartCoroutine(Finished());
+        }
+    }
+
+    IEnumerator Finished()
+    {
+        if (!done)
+        {
+            done = true;
+            yield return new WaitForSeconds(0.1f);
+            FindObjectOfType<NavMeshAgent>().destination = ariadnePos.position;
+            FindObjectOfType<DialogueRunner>().StartDialogue("AriadneMusic");
         }
     }
 
