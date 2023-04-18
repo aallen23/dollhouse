ab0c593a80dbf682a1f0ec5d827bfaec58e3a8d7
Ben <benhunsicker1@gmail.com>
2023-04-18 13:46:37 -0400

    fixed umbrella puzzle

--- Assets/Input/Puzzles/UmbrellaModelSwitcher.cs
+++ Assets/Input/Puzzles/UmbrellaModelSwitcher.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UmbrellaModelSwitcher : MonoBehaviour
 {
     public List<GameObject> umbrellaModels;
+    public UmbrellaPuzzle puzzle;
 
     public void SwitchModel(int item)
     {
@@ -13,6 +14,7 @@ public class UmbrellaModelSwitcher : MonoBehaviour
             if (umbrellaModels.IndexOf(umb) == item)
             {
                 umb.SetActive(true);
+                puzzle.UpdateUmbrellas(gameObject.name, umb.GetComponent<ObjectData>().addedItem);
             }
             else
             {
