8fcbd399b2b88467e71a5e3a24f02877acb42ec3
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2023-08-03 18:49:33 -0400

    MinMaxSlider Package Removed

--- Assets/UIStuff/Scripts/CharacterData.cs
+++ Assets/UIStuff/Scripts/CharacterData.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GD.MinMaxSlider;
 
 [CreateAssetMenu(fileName = "New Character", menuName = "Character", order = 2)]
 public class CharacterData : ScriptableObject
@@ -11,5 +10,5 @@ public class CharacterData : ScriptableObject
     [Tooltip("The character controller for animation.")] public RuntimeAnimatorController characterController;
 
     [Tooltip("If true, the sprite can blink.")] public bool animateBlink = true;
-    [MinMaxSlider(0, 30), Tooltip("The random range (in seconds) between blinking.")] public Vector2 blinkingFrequency = new Vector2(1, 5);
+    [Tooltip("The random range (in seconds) between blinking.")] public Vector2 blinkingFrequency = new Vector2(1, 5);
 }
